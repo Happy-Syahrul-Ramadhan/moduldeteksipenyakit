bc2dab2eb913fca44f13d8fc41fa0e2e04db578d
syahrul <syahrul19112003@gmail.com>
2023-11-28 20:21:26 +0700

    initial commit

--- Home.py
+++ Home.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title('Selamat Datang di Modul Untuk Mendeteksi Jenis Penyakit Berdasarkan Gejala')
+# t.title('Selamat Datang di Modul Untuk Mendeteksi Jenis Penyakit')
+
+local_image_path = 'doctor.png'
+
+col1, col2 = st.columns(2)
+
+col2.image(local_image_path, width=420)
+col1.markdown("<h1 style='text-align: center; margin-top='10px'>Modul Deteksi Penyakit</h1>", unsafe_allow_html=True)
+col1.write("Modul ini adalah modul yang mampu menganalisis berbagai jenis data penyakit, mulai dari gambaran medis hingga parameter klinis, dengan akurasi tinggi. Selain itu, penggunaan modul ini dapat mempercepat proses diagnostik, memberikan respon cepat terhadap kondisi kesehatan, dan pada akhirnya meningkatkan hasil perawatan pasien.")
